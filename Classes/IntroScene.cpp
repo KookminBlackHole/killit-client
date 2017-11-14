a1d144f7676513857dd1a6b591c924f0eb26219d
Leejooyoon <ljy9795@naver.com>
2017-11-14 15:36:17 +0900

    인트로 효과 수정
    Signed-off-by: Leejooyoon <ljy9795@naver.com>

--- Classes/IntroScene.cpp
+++ Classes/IntroScene.cpp
@@ -5,7 +5,7 @@
 USING_NS_CC;
 
 bool IntroScene::init() {
-	LayerColor *background = LayerColor::create(Color4B::WHITE);
+	LayerColor *background = LayerColor::create(Color4B::BLACK);
 	this->addChild(background);
 
 	Size size = Director::getInstance()->getVisibleSize();
@@ -14,13 +14,13 @@ bool IntroScene::init() {
 	NodeGrid *grid = NodeGrid::create();
 	this->addChild(grid);
 
-	Sprite *image = Sprite::create("res/logo.png");
+	Sprite *image = Sprite::create("res/logo_black.png");
 	image->getTexture()->setAliasTexParameters();
 	image->setPosition(origin);
 	image->setScale(2);
 	grid->addChild(image);
 
-	grid->runAction(Sequence::create(Ripple3D::create(3.0f, Size(100, 100), origin, 350, 3, 100.0f), CallFunc::create([&] {
+	grid->runAction(Sequence::create(Ripple3D::create(3.0f, Size(100, 100), origin, 200, 3, 100.0f), CallFunc::create([&] {
 
 	Director::getInstance()->replaceScene(TransitionFade::create(2.0f, HelloWorld::create()));
 
