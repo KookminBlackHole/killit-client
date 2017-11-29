9ac8c81fcbae401b15d95f2a0b35956eeb42b296
Leejooyoon <ljy9795@naver.com>
2017-11-29 10:04:45 +0900

    이미지 리소스 업로드
    Signed-off-by: Leejooyoon <ljy9795@naver.com>

---
+++ Classes/MainMenu.h
@@ -0,0 +1,14 @@
+#ifndef _MAINMENU_MENU_H_
+#define _MAINMENU_MENU_H_
+
+#include "cocos2d.h"
+
+class IntroScene : public cocos2d::Scene {
+public:
+	bool init();
+
+	CREATE_FUNC(IntroScene);
+};
+
+#endif
+
